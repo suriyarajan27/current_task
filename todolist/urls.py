8f644d4715034bd28ff1345f9196e5f98d6258ff
suriyarajan27 <sksuriyarajan@gmail.com>
2025-10-26 11:01:38 +0530

    make changes

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signn, name='signn.html'),
-    path('loginn/', views.loginn, name='loginn.html'),
-    path('todopath/',views.todopath, name='todopath.html'),
+    path('', views.signn, name='signn'),
+    path('loginn/', views.loginn, name='loginn'),
+    path('todopath/',views.todopath, name='todopath'),
     path('DeleteTask/<str:name>/', views.DeleteTask, name='Delete'),
     path('logout/', views.LogoutView, name='logout'),
     path('UpdateTask/<str:name>/', views.UpdateTask, name='Update'),
--- todolist/views.py
+++ todolist/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import HttpResponse
 
 def signn(request):
     if request.user.is_authenticated:
-        return redirect('todopath.html')
+        return redirect('todopath')
     
     if request.method=="POST":
         fnm=request.POST.get('fnm')
@@ -21,34 +21,34 @@ def signn(request):
 
         if len(pwd) <3:
             messages.error(request, 'Password must be more than three character')
-            return redirect('signn.html')
+            return redirect('signn')
         
         if len(fnm) < 5:
             messages.error(request, 'username should contain more than five character')
-            return redirect('signn.html')
+            return redirect('signn')
         
         # this function is written to address an error message when the user does'nt have an sign account
 
         get_all_user_by_username = User.objects.filter(username=fnm)
         if get_all_user_by_username:
             messages.error(request, 'Entered username already exsits')
-            return redirect('signn.html')
+            return redirect('signn')
         
         get_all_user_by_email = User.objects.filter(email=emailid)
         if get_all_user_by_email:
             messages.error(request, 'Entered email already exsits')
-            return redirect('signn.html')
+            return redirect('signn')
 
         sign_user=User.objects.create_user(fnm, emailid, pwd)
         sign_user.save()
 
         messages.success(request, 'user is created successfully')
-        return redirect('loginn.html')
-    return render(request, 'signn.html', {})
+        return redirect('loginn')
+    return render(request, 'signn')
 
 def loginn(request):
     if request.user.is_authenticated:
-        return redirect('todopath.html')
+        return redirect('todopath')
 
     if request.method=="POST":
         fnm=request.POST.get('fnm')
@@ -57,13 +57,13 @@ def loginn(request):
         login_user = authenticate(username=fnm, password=pwd)
         if login_user is None:
             messages.error(request, 'user does not exsit, create an new user')
-            return redirect('signn.html')
+            return redirect('signn')
         else:
             login(request, login_user)
             messages.error(request, 'hi welcome')
-            return redirect('todopath.html')
+            return redirect('todopath')
         
-    return render(request, 'loginn.html')
+    return render(request, 'loginn')
 
 @login_required
 def todopath(request):
@@ -78,21 +78,21 @@ def todopath(request):
 
         'todos':all_todos
     }
-    return render(request, 'todopath.html', context)
+    return render(request, 'todopath', context)
 
 @login_required
 def DeleteTask(request, name):
     get_task = TODOTASK.objects.get(user=request.user, task_name=name)
     get_task.delete()
-    return redirect('todopath.html')
+    return redirect('todopath')
 
 @login_required
 def UpdateTask(request, name):
     get_task = TODOTASK.objects.get(user=request.user, task_name=name)
     get_task.status = True
     get_task.save()
-    return redirect('todopath.html')
+    return redirect('todopath')
 
 def LogoutView(request):
     logout(request)
-    return redirect('signn.html')
+    return redirect('signn')
